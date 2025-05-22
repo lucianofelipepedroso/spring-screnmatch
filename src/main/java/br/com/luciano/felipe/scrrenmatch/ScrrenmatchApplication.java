736eea3ee8b94919ac3c4bd658707e0b1187b7bf
Luciano Felipe Pedroso <luciano.pedroso@live.com>
2025-05-22 19:10:37 -0300

    atualizacao codigo

--- src/main/java/br/com/luciano/felipe/scrrenmatch/ScrrenmatchApplication.java
+++ src/main/java/br/com/luciano/felipe/scrrenmatch/ScrrenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.luciano.felipe.scrrenmatch;
 
+import br.com.luciano.felipe.scrrenmatch.model.DadosSerie;
 import br.com.luciano.felipe.scrrenmatch.service.ConsumoAPI;
+import br.com.luciano.felipe.scrrenmatch.service.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,10 +19,11 @@ public class ScrrenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		ConsumoAPI api = new ConsumoAPI();
+		ConverterDados converterDados = new ConverterDados();
 		String json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-		json = api.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
+		DadosSerie dadosSerie = converterDados.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
+
 
 	}
 }
